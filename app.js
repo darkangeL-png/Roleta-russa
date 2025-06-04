2df7f72c765ca2b2cd65edb23aeaf3a64cc7b62d
darkangeL-png <geovaniiamstark@gmail.com>
2025-06-04 17:42:54 -0300

    commit-2

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-const BalaLocalizacao = parseInt(Math.random() * 6 + 1);
-console.log("A bala está na posição: " + BalaLocalizacao);
+// const BalaLocalizacao = parseInt(Math.random() * 6 + 1);
+// const Bala = 0;
+// const btnAtirar = document.querySelector(".main_atirar");
+
+// console.log(btnAtirar);
